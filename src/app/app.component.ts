ca22c0e32fffc5d2794d410f7d28176210c60cec
faisal-tahseen <faisal@asiansol.com>
2025-07-11 16:58:39 +0500

    Refactor import paths for Register and Login components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { RegisterComponent } from './Components/Auth/register/register.component';
-import { LoginComponent } from "./Components/Auth/login/login.component";
+import { RegisterComponent } from './Components/Pages/Auth/register/register.component';
+import { LoginComponent } from "./Components/Pages/Auth/login/login.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
